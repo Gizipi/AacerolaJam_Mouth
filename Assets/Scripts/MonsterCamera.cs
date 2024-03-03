97899551231a48e1b33c053c27aaac3a44431fe9
Nathaniel <nathaniel.atkinson@playsidestudios.com>
2024-03-04 08:56:28 +1000

    Job: Created Monster camera movement

---
+++ Assets/Scripts/MonsterCamera.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCamera : MonoBehaviour
+{
+
+    private bool _isPathing = false;
+
+    [SerializeField]private GameObject _target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StareAtTarget();
+    }
+
+    private void SetTarget(GameObject target)
+    {
+        _target = target;
+    }
+
+    public void SetPathingState(bool state)
+    {
+        _isPathing = state;
+    }
+
+
+    private void StareAtTarget()
+    {
+        if (_isPathing || _target == null)
+            return;
+
+        this.transform.LookAt(_target.transform.position);
+    }
+
+
+
+}
